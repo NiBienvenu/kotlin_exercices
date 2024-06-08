d946e7af997b227a6a0f443035f742ab05aca733
“NiBienvenu” <nicibienvenu@gmail.com>
2024-06-08 13:49:09 +0200

    premier point

---
+++ main.kt
@@ -0,0 +1,38 @@
+
+fun main(){
+    //commenattaire
+    /**
+     * @author NiBienvenu
+     * @date 2024-06-08
+     *
+     */
+    /*
+    ghjgklhmno,nm
+    jknbm
+
+    */
+    //Types donnees en kotlin
+    /*
+     val a:Int = 10
+    val b:Double = 10.0
+    val c:String = "Hello World"
+    val d:Boolean = true
+    val e:Char = 'a'
+    val f:Float = 10.0f
+    */
+    // differences entre val et var
+
+    val a:Int = 10 // const : javascript, final // java ou dart
+    var b:Int = 10
+//    a = 20
+    b = 20
+    var x = 5
+    x=24
+    val y ="somme $a + $b = ${a+b}"
+
+    // affichage en kotlin
+    println(y);
+
+
+    println("Hello World"+a)
+}
