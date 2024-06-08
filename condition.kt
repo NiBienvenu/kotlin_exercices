29efc97ce20d7cfdaa5c081eda650fa8c1da1a40
“NiBienvenu” <nicibienvenu@gmail.com>
2024-06-08 14:24:22 +0200

    les listes et ses fonction

--- condition.kt
+++ condition.kt
@@ -1,6 +1,28 @@
 
 
 fun main(args: Array<String>) {
-    // les conditions en kotlin
+    // les conditions if et swith en kotlin
+     var a:Int = 30
+    var b:Int = 26
+    if (a ==b){
+        println("a et b sont egaux")
+    }else{
+        println("a et b ne sont pas egaux")
+    }
+    when(a){
+        10->println("a egal a 10")
+//        "bo"->println("a egal a bo")
+        30->println("a egal a 30")
+        else->println("a n'est pas egal a 10, 20 ou 30")
+    }
+
+    var result = if (a > b){
+        a-b
+    }else{
+        b-a
+    }
+
+println(result)
+
 
 }
