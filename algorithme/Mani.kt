17ce9fdf11060db314de21f064ed7b8dcf1b334b
NiBienvenu <nicibienvenu@gmail.com>
2025-03-04 07:07:32 +0200

    alogoritme exercie 1 & 2

--- algorithme/Mani.kt
+++ algorithme/Mani.kt
@@ -1,5 +1,24 @@
 package algorithme
 
 fun main() {
-    println("Hello word")
+
+
+    println(" somme de 1 à 8 ${sommeN(10)}")
+
+}
+
+fun changeV(){
+    var a = 5;
+    var b = 6;
+
+    a = a+b
+    b = a-b
+    a = a-b
+
+    println("a est $a  b est $b")
+}
+
+// Exercies 2
+fun sommeN(n : Int) : Int{
+    return (n * (n + 1)) / 2
 }
